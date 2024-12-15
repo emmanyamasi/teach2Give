f4a551a29a9d7823ef881e6dd4d8a675734247cc
emmanyamasi <2004emmanyamasi@gmail.com>
2024-12-15 11:39:07 +0300

    init commit

---
+++ app.py
@@ -0,0 +1,17 @@
+#function for capitalizing first letter
+
+def capitalize(message):
+    result = message.title()
+    return result
+
+
+
+
+
+
+
+
+#usage
+message = input("Enter your message : ")
+response = capitalize(message)
+print("message :" , response)
